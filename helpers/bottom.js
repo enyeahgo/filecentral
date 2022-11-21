add61f94f28551235c6872bc27dbe67fac8ec84a
Inigo Orosco <enyeahgo@gmail.com>
2022-11-21 15:03:09 +0800

    New Commit

---
+++ helpers/bottom.js
@@ -0,0 +1,15 @@
+let bottom = `
+      </div>
+    </div>
+  </div>
+
+  <script src="/jquery/dist/jquery.min.js"></script>
+  <script src="/sweetalert2/dist/sweetalert2.all.min.js"></script>
+  <script src="/axios/dist/axios.min.js"></script>
+  <script src="/bootstrap/dist/js/bootstrap.min.js"></script>
+  <script src="/socket.io/socket.io.min.js"></script>
+  <script src="/paginationjs/dist/pagination.js"></script>
+  <script src="/myfunctions.js"></script>
+	`;
+
+module.exports = bottom;
