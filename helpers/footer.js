1ce7d7c53f45ff2bd9482e6f978acf778289b2a9
Inigo Orosco <enyeahgo@gmail.com>
2023-02-02 14:08:54 +0800

    New Commit

--- helpers/footer.js
+++ helpers/footer.js
@@ -1,7 +1,7 @@
 let footer = `
     <footer id="footer">
       <div class="py-4 my-4 text-center border-top">
-        <span style="font-family: 'Courier New', Courier, monospace; font-size: 16px; font-weight: bold;">FILE CENTRAL &copy; <script>document.write((new Date).toLocaleDateString('en-PH', { year: 'numeric' }))</script></span><br>
+        <span style="font-family: 'Courier New', Courier, monospace; font-size: 16px; font-weight: bold;">VANGUARD &copy; <script>document.write((new Date).toLocaleDateString('en-PH', { year: 'numeric' }))</script></span><br>
         <span class="sm">Enliven Technologies Philippines<br>All rights reserved</span>
       </div>
     </footer>
